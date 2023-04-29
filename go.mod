21b15bb6089aebbad6759abebe4462e0738343ce
coy001 <coy001@ucsd.edu>
2023-04-29 05:26:02 +0000

    build a service can send request to server the client can get feedback from the server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/coy001/MovieRecommender
+
+go 1.20
--- start_docker.sh
+++ start_docker.sh
@@ -1,1 +1,1 @@
-docker run -v '/Users/luanqinglin/desktop/MovieRecommender':'/Users/luanqinglin/desktop/MovieRecommender' -w '/Users/luanqinglin/desktop/MovieRecommender' -i -t hello_world /bin/bash  
+docker run --rm -p 3333:3333 -v '/Users/luanqinglin/desktop/MovieRecommender':'/Users/luanqinglin/desktop/MovieRecommender' -w '/Users/luanqinglin/desktop/MovieRecommender' -i -t hello_world /bin/bash  
