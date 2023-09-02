03563c36f9bafec9dd72492f418e8b48841d69d2
coy001 <coy001@ucsd.edu>
2023-09-01 19:53:45 -0700

    Delete notebook updates

--- torch/buildJupyter.sh
+++
@@ -1,8 +0,0 @@
-docker build --no-cache \
-             -f create-user.dockerfile \
-             --build-arg MY_UID="$(id -u)" \
-             --build-arg MY_GID="$(id -g)" \
-             --build-arg USER=demo \
-             --build-arg HOME=/home/demo \
-             -t tverous/pytorch-notebook:user \
-             .
--- torch/runJupyter.sh
+++
@@ -1,5 +0,0 @@
-docker run --rm -it  \
-           -p 8888:8888  \
-	   -v '/Users/luanqinglin/desktop/MovieRecommender/torch':'/Users/luanqinglin/desktop/MovieRecommender/torch' -w '/Users/luanqinglin/desktop/MovieRecommender/torch' \
-           -e JUPYTER_TOKEN=passwd  \
-           tverous/pytorch-notebook:latest
